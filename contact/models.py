e74c7cc0d662a523ca5a5a3a1da6ece458d23354
Edinaildo <edinaildopereira2013@gmail.com>
2023-02-09 22:55:49 -0300

    cria class models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ContactBook(models.Model):
+    image = models.FileField(_("Avatar Image"),)
+    mobile_number = models.CharField(_("Mobile Number"), max_length=11)
+    email = models.EmailField(_("user Email"), max_length=254)
+    label = models.CharField(_("label"), max_length=50)
+    person_name = models.CharField(_("person Name"), max_length=50)
+    last_updated = models.DateTimeField(_("Last Updated"), auto_now=True)
+    created = models.DateTimeField(_("Created"), auto_now_add=True)
+
+
+
+    class Meta:
+        verbose_name = _("ContactBook")
+        verbose_name_plural = _("ContactBooks")
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reversed("ContactBook_detail", kwargs=("pk": self.pk))
